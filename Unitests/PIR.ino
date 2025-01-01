e17b9d903766f1a853447be587dfe21ab51f851d
David Nisanov <96836742+DavidNis@users.noreply.github.com>
2025-01-01 16:50:44 +0200

    Add files via upload

---
+++ Unitests/PIR.ino
@@ -0,0 +1,19 @@
+
+int pirPin = 13; // PIR pin 
+
+int pirStat = 0; // PIR status
+
+void setup() {
+   
+ Serial.begin(9600);
+}
+void loop(){
+  delay(200);
+ pirStat = digitalRead(pirPin); 
+ if (pirStat == HIGH) {            // if motion detected
+   Serial.println("Motion detected!!!");
+ } 
+ else {
+   Serial.println("No motion!");
+ }
+} 
