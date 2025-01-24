fa26563fd166c3e30c08f2ff5a31ff47f2716fee
David Nisanov <96836742+DavidNis@users.noreply.github.com>
2025-01-24 02:27:29 +0200

    Update timer_manager.dart

--- App/lib/Utils/timer_manager.dart
+++ App/lib/Utils/timer_manager.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
+import 'package:firebase_database/firebase_database.dart';
 
 class TimerManager {
   static final TimerManager _instance = TimerManager._internal();
@@ -9,6 +10,8 @@ class TimerManager {
   int _remainingSeconds = 0;
   bool _isRunning = false;
   final ValueNotifier<int> remainingSecondsNotifier = ValueNotifier(0);
+
+  final DatabaseReference _databaseRef = FirebaseDatabase.instance.ref(); // Firebase reference
 
   TimerManager._internal();
 
@@ -25,6 +28,7 @@ class TimerManager {
         remainingSecondsNotifier.value = _remainingSeconds;
       } else {
         stopTimer(); // Stop the timer and reset state
+        _updateFirebaseForACOff(); // Turn off the AC in Firebase
         onTimerEnd();
       }
     });
@@ -40,4 +44,16 @@ class TimerManager {
   bool isRunning() => _isRunning;
 
   int getRemainingSeconds() => _remainingSeconds;
+
+  Future<void> _updateFirebaseForACOff() async {
+    try {
+      await _databaseRef.child("transmitter/onOff").set({
+        "code": "F740BF", // IR code for turning off
+        "value": "Off"
+      });
+      print("AC turned off in Firebase.");
+    } catch (e) {
+      print("Failed to update Firebase for AC off: $e");
+    }
+  }
 }
