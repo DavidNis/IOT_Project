c273df276730d0e135ce8f86f1e4c17d426eb034
David Nisanov <96836742+DavidNis@users.noreply.github.com>
2025-01-26 03:35:17 +0200

    Update main.dart

--- App/lib/main.dart
+++ App/lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'Screens/login_screen.dart'; // Import the LoginScreen
+import 'Screens/login_screen.dart'; 
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
